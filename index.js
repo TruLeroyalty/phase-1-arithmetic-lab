f7cbf3dd865b071ebaae3363ba1abe7997181100
Leroy Scott Jr <leroysjr@yahoo.com>
2023-03-18 14:52:40 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10);
+let num4 = 1.5;
+let num3 = 6;
+let mod = num3 / num4;
+let max = Math.max(2,4,5,6,20);
